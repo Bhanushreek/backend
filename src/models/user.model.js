e9e1020f3ff593f5366ed72a74186bdff685f50b
Bhanushreek <kundurbhanushreereddy@gmail.com>
2025-01-05 15:13:41 +0530

    user controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, {Schema} from "mongoose";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
@@ -9,28 +9,28 @@ const userSchema = new Schema(
             required: true,
             unique: true,
             lowercase: true,
-            trim: true,
+            trim: true, 
             index: true
         },
         email: {
             type: String,
             required: true,
             unique: true,
-            lowercase: true,
-            trim: true,
+            lowecase: true,
+            trim: true, 
         },
         fullName: {
             type: String,
             required: true,
-            trim: true,
+            trim: true, 
             index: true
         },
         avatar: {
-            type: String, //cloudinary url
-            required: true
+            type: String, // cloudinary url
+            required: true,
         },
         coverImage: {
-            type: String, //cloudinary url
+            type: String, // cloudinary url
         },
         watchHistory: [
             {
@@ -45,7 +45,11 @@ const userSchema = new Schema(
         refreshToken: {
             type: String
         }
-    },{timestamps: true}
+
+    },
+    {
+        timestamps: true
+    }
 )
 
 userSchema.pre("save", async function (next) {
@@ -55,17 +59,17 @@ userSchema.pre("save", async function (next) {
     next()
 })
 
-userSchema.methods.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
 userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
-            _id:this._id,
+            _id: this._id,
             email: this.email,
             username: this.username,
-            fullname: this.fullName
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
@@ -76,7 +80,8 @@ userSchema.methods.generateAccessToken = function(){
 userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
-            _id:this._id
+            _id: this._id,
+            
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
